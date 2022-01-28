918e82a7c7eecabd3db198bef85550a3b4cabe88
Simsimhan Chobo <jun32820@gmail.com>
2022-01-28 09:40:10 +0900

    bug fixed

--- Assets/SC KRM/Editor/Game Object Add Menu/GameObjectAddMenu.cs
+++ Assets/SC KRM/Editor/Game Object Add Menu/GameObjectAddMenu.cs
@@ -9,7 +9,11 @@ public class GameObjectAddMenu
     public static void SettingInputField(MenuCommand menuCommand)
     {
         GameObject gameObject = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/SC KRM/Editor/Game Object Add Menu/Setting Input Field.prefab", typeof(GameObject));
-        gameObject = Object.Instantiate(gameObject, ((GameObject)menuCommand.context).transform);
+        if (menuCommand.context != null)
+            gameObject = Object.Instantiate(gameObject, ((GameObject)menuCommand.context).transform);
+        else
+            gameObject = Object.Instantiate(gameObject, null);
+
         gameObject.name = "Setting Input Field";
 
         Undo.RegisterCreatedObjectUndo(gameObject, "Create " + gameObject.name);
@@ -20,7 +24,11 @@ public class GameObjectAddMenu
     public static void SettingSlider(MenuCommand menuCommand)
     {
         GameObject gameObject = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/SC KRM/Editor/Game Object Add Menu/Setting Slider.prefab", typeof(GameObject));
-        gameObject = Object.Instantiate(gameObject, ((GameObject)menuCommand.context).transform);
+        if (menuCommand.context != null)
+            gameObject = Object.Instantiate(gameObject, ((GameObject)menuCommand.context).transform);
+        else
+            gameObject = Object.Instantiate(gameObject, null);
+
         gameObject.name = "Setting Slider";
 
         Undo.RegisterCreatedObjectUndo(gameObject, "Create " + gameObject.name);
@@ -31,7 +39,11 @@ public class GameObjectAddMenu
     public static void SettingToggle(MenuCommand menuCommand)
     {
         GameObject gameObject = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/SC KRM/Editor/Game Object Add Menu/Setting Toggle.prefab", typeof(GameObject));
-        gameObject = Object.Instantiate(gameObject, ((GameObject)menuCommand.context).transform);
+        if (menuCommand.context != null)
+            gameObject = Object.Instantiate(gameObject, ((GameObject)menuCommand.context).transform);
+        else
+            gameObject = Object.Instantiate(gameObject, null);
+
         gameObject.name = "Setting Toggle";
 
         Undo.RegisterCreatedObjectUndo(gameObject, "Create " + gameObject.name);
