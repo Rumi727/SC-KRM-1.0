dd66d16cffadad2e9a1d44f772b794428885a11b
Simsimhan Chobo <jun32820@gmail.com>
2022-04-14 21:48:47 +0900

    Fix fade in out bug

--- Assets/SC KRM/UI/Setting/SettingClass.cs
+++ Assets/SC KRM/UI/Setting/SettingClass.cs
@@ -138,12 +138,18 @@ namespace SCKRM.UI.Setting
                 resetButton.interactable = false;
                 nameText.offsetMin = nameText.offsetMin.Lerp(new Vector2(0, nameText.offsetMin.y), 0.2f * Kernel.fpsUnscaledDeltaTime);
                 resetButton.alpha = resetButton.alpha.Lerp(0, 0.2f * Kernel.fpsUnscaledDeltaTime);
+
+                if (resetButton.alpha < 0.01f)
+                    resetButton.alpha = 0;
             }
             else
             {
                 resetButton.interactable = true;
                 nameText.offsetMin = nameText.offsetMin.Lerp(new Vector2(22, nameText.offsetMin.y), 0.2f * Kernel.fpsUnscaledDeltaTime);
                 resetButton.alpha = resetButton.alpha.Lerp(1, 0.2f * Kernel.fpsUnscaledDeltaTime);
+
+                if (resetButton.alpha > 0.99f)
+                    resetButton.alpha = 1;
             }
         }
 
