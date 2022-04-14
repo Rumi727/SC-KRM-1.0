1afda0137cfaa02366eca34345acf768c4d50dcf
Simsimhan Chobo <jun32820@gmail.com>
2022-04-14 21:43:24 +0900

    Message Box UI completion

--- Assets/SC KRM/Window/WindowManager.cs
+++ Assets/SC KRM/Window/WindowManager.cs
@@ -2,10 +2,16 @@ using Cysharp.Threading.Tasks;
 using SCKRM.Input;
 using SCKRM.Object;
 using SCKRM.Renderer;
+using SCKRM.Resource;
 using SCKRM.Tool;
+using SCKRM.UI;
+using SCKRM.UI.StatusBar;
 using System;
+using System.Linq;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace SCKRM.Window
 {
@@ -17,13 +23,21 @@ namespace SCKRM.Window
         {
             if (isMessageBoxShow)
             {
-                messageBoxCanvasGroup.alpha = messageBoxCanvasGroup.alpha.MoveTowards(1, 0.01f * Kernel.fpsDeltaTime);
+                messageBoxCanvasGroup.alpha = messageBoxCanvasGroup.alpha.Lerp(1, 0.2f * Kernel.fpsDeltaTime);
+                if (messageBoxCanvasGroup.alpha > 0.99f)
+                    messageBoxCanvasGroup.alpha = 1;
+
                 messageBoxCanvasGroup.interactable = true;
+                messageBoxCanvasGroup.blocksRaycasts = true;
             }
             else
             {
-                messageBoxCanvasGroup.alpha = messageBoxCanvasGroup.alpha.MoveTowards(0, 0.01f * Kernel.fpsDeltaTime);
+                messageBoxCanvasGroup.alpha = messageBoxCanvasGroup.alpha.Lerp(0, 0.2f * Kernel.fpsDeltaTime);
+                if (messageBoxCanvasGroup.alpha < 0.01f)
+                    messageBoxCanvasGroup.alpha = 0;
+
                 messageBoxCanvasGroup.interactable = false;
+                messageBoxCanvasGroup.blocksRaycasts = false;
             }
         }
 
@@ -178,22 +192,41 @@ namespace SCKRM.Window
         [SerializeField] CanvasGroup messageBoxCanvasGroup;
         [SerializeField] Transform messageBoxButtons;
         [SerializeField] CustomAllTextRenderer messageBoxInfo;
+        [SerializeField] CustomAllSpriteRenderer messageBoxIcon;
+        [SerializeField] MessageBoxButton messageBoxButtonPrefab;
 
         static MessageBoxButton[] createdMessageBoxButton = new MessageBoxButton[0];
         public static bool isMessageBoxShow { get; private set; } = false;
 
-        public static async UniTask<int> MessageBox(NameSpacePathPair buttons, int defaultIndex, string text, string nameSpace = "") => await messageBox(new NameSpacePathPair[] { buttons }, defaultIndex, text, nameSpace, new ReplaceOldNewPair[0]);
-        public static async UniTask<int> MessageBox(NameSpacePathPair buttons, int defaultIndex, string text, string nameSpace, ReplaceOldNewPair replace) => await messageBox(new NameSpacePathPair[] { buttons }, defaultIndex, text, nameSpace, new ReplaceOldNewPair[] { replace });
-        public static async UniTask<int> MessageBox(NameSpacePathPair buttons, int defaultIndex, string text, string nameSpace, ReplaceOldNewPair[] replace) => await messageBox(new NameSpacePathPair[] { buttons }, defaultIndex, text, nameSpace, replace);
-        public static async UniTask<int> MessageBox(NameSpacePathPair[] buttons, int defaultIndex, string text, string nameSpace = "") => await messageBox(buttons, defaultIndex, text, nameSpace, new ReplaceOldNewPair[0]);
-        public static async UniTask<int> MessageBox(NameSpacePathPair[] buttons, int defaultIndex, string text, string nameSpace, ReplaceOldNewPair replace) => await messageBox(buttons, defaultIndex, text, nameSpace, new ReplaceOldNewPair[] { replace });
-        public static async UniTask<int> MessageBox(NameSpacePathPair[] buttons, int defaultIndex, string text, string nameSpace, ReplaceOldNewPair[] replace) => await messageBox(buttons, defaultIndex, text, nameSpace, replace);
+        public static async UniTask<int> MessageBox(NameSpacePathPair button, int defaultIndex, NameSpacePathPair info, NameSpacePathPair icon) => await messageBox(new NameSpacePathPair[] { button }, defaultIndex, info, icon, new ReplaceOldNewPair[0]);
+        public static async UniTask<int> MessageBox(NameSpacePathPair button, int defaultIndex, NameSpacePathPair info, NameSpacePathPair icon, ReplaceOldNewPair replace) => await messageBox(new NameSpacePathPair[] { button }, defaultIndex, info, icon, new ReplaceOldNewPair[] { replace });
+        public static async UniTask<int> MessageBox(NameSpacePathPair button, int defaultIndex, NameSpacePathPair info, NameSpacePathPair icon, ReplaceOldNewPair[] replace) => await messageBox(new NameSpacePathPair[] { button }, defaultIndex, info, icon, replace);
+        public static async UniTask<int> MessageBox(NameSpacePathPair[] buttons, int defaultIndex, NameSpacePathPair info, NameSpacePathPair icon) => await messageBox(buttons, defaultIndex, info, icon, new ReplaceOldNewPair[0]);
+        public static async UniTask<int> MessageBox(NameSpacePathPair[] buttons, int defaultIndex, NameSpacePathPair info, NameSpacePathPair icon, ReplaceOldNewPair replace) => await messageBox(buttons, defaultIndex, info, icon, new ReplaceOldNewPair[] { replace });
+        public static async UniTask<int> MessageBox(NameSpacePathPair[] buttons, int defaultIndex, NameSpacePathPair info, NameSpacePathPair icon, ReplaceOldNewPair[] replace) => await messageBox(buttons, defaultIndex, info, icon, replace);
 
-        static async UniTask<int> messageBox(NameSpacePathPair[] buttons, int defaultIndex, string text, string nameSpace, ReplaceOldNewPair[] replace)
+        static async UniTask<int> messageBox(NameSpacePathPair[] buttons, int defaultIndex, NameSpacePathPair info, NameSpacePathPair icon, ReplaceOldNewPair[] replace)
         {
+            await UniTask.WaitUntil(() => instance != null);
+
             if (isMessageBoxShow)
                 return defaultIndex;
+            else if (defaultIndex < 0 || buttons.Length < defaultIndex)
+                return defaultIndex;
 
+            isMessageBoxShow = true;
+
+            instance.messageBoxIcon.nameSpace = icon.nameSpace;
+            instance.messageBoxIcon.path = icon.path;
+            instance.messageBoxIcon.Refresh();
+
+            instance.messageBoxInfo.replace = replace;
+
+            instance.messageBoxInfo.nameSpace = info.nameSpace;
+            instance.messageBoxInfo.path = info.path;
+            instance.messageBoxInfo.Refresh();
+
+            #region Button Object Create
             for (int i = 0; i < createdMessageBoxButton.Length; i++)
                 ObjectPoolingSystem.ObjectRemove("window_manager.message_box_button", createdMessageBoxButton[i]);
 
@@ -203,28 +236,77 @@ namespace SCKRM.Window
                 MessageBoxButton button = (MessageBoxButton)ObjectPoolingSystem.ObjectCreate("window_manager.message_box_button", instance.messageBoxButtons);
                 createdMessageBoxButton[i] = button;
 
-                instance.messageBoxInfo.replace = replace;
-
-                instance.messageBoxInfo.nameSpace = nameSpace;
-                instance.messageBoxInfo.path = text;
-
                 button.index = i;
 
                 button.text.nameSpace = buttons[i].nameSpace;
                 button.text.path = buttons[i].path;
                 button.text.Refresh();
 
+                //버튼이 눌렸을때, UI를 닫기 위해서 버튼에 있는 OnClick 이벤트에 clickedIndex를 button.index로 바꾸는 action 메소드를 추가합니다
                 button.button.onClick.AddListener(() => action(button));
             }
 
-            int clickedIndex = -1;
-            if (await UniTask.WaitUntil(() => InputManager.GetKey("gui.back", InputType.Down, "all") || clickedIndex >= 0, PlayerLoopTiming.Update, AsyncTaskManager.cancelToken).SuppressCancellationThrow())
-                return defaultIndex;
-            else if (clickedIndex < 0)
-                return defaultIndex;
-            else
-                return clickedIndex;
+            for (int i = 0; i < createdMessageBoxButton.Length; i++)
+            {
+                Navigation navigation = new Navigation();
+                navigation.mode = Navigation.Mode.Explicit;
 
+                if (i > 0)
+                    navigation.selectOnUp = createdMessageBoxButton[i - 1].button;
+                if (i < createdMessageBoxButton.Length - 1)
+                    navigation.selectOnDown = createdMessageBoxButton[i + 1].button;
+
+                createdMessageBoxButton[i].button.navigation = navigation;
+            }
+            #endregion
+
+            StatusBarManager.tabSelectGameObject = createdMessageBoxButton[defaultIndex].gameObject;
+            EventSystem.current.SetSelectedGameObject(createdMessageBoxButton[defaultIndex].gameObject);
+
+            InputManager.forceInputLock = true;
+
+
+
+            int clickedIndex = -1;
+
+            //Back 버튼을 눌렀을때, UI를 닫기 위해 이벤트에 clickedIndex를 defaultIndex로 변경하는 BackEvent 메소드를 추가합니다
+            UIManager.BackEventAdd(backEvent, true);
+
+            GameObject oldSelectedGameObject = null;
+            while (clickedIndex < 0)
+            {
+                /*
+                 * BackEvent 함수가 호출되거나, 버튼이 눌려서 clickedIndex가 0 이상이 되거나
+                 * select 함수가 호출되서 함수가 리턴 될 때까지 대기합니다
+                 */
+
+                if (EventSystem.current.currentSelectedGameObject == null)
+                    EventSystem.current.SetSelectedGameObject(oldSelectedGameObject);
+                else
+                    oldSelectedGameObject = EventSystem.current.currentSelectedGameObject;
+
+                if (await UniTask.DelayFrame(1, PlayerLoopTiming.Update, AsyncTaskManager.cancelToken).SuppressCancellationThrow())
+                    return 0;
+            }
+
+            return select(clickedIndex);
+
+
+
+            int select(int index)
+            {
+                isMessageBoxShow = false;
+                StatusBarManager.tabSelectGameObject = null;
+                EventSystem.current.SetSelectedGameObject(null);
+
+                InputManager.forceInputLock = false;
+
+                UIManager.BackEventRemove(backEvent, true);
+
+                return index;
+            }
+
+            void backEvent() => clickedIndex = defaultIndex;
             void action(MessageBoxButton messageBoxButton) => clickedIndex = messageBoxButton.index;
         }
     }
