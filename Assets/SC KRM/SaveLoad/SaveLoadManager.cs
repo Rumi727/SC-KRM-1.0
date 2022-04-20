0fbf9951454389f85c1167aa58cb643617f5972f
Simsimhan Chobo <jun32820@gmail.com>
2022-04-21 01:19:38 +0900

    Change the save data to be customizable

--- Assets/SC KRM/SaveLoad/SaveLoadManager.cs
+++ Assets/SC KRM/SaveLoad/SaveLoadManager.cs
@@ -30,7 +30,7 @@ namespace SCKRM.SaveLoad
 
     public static class SaveLoadManager
     {
-        public static List<SaveLoadClass> generalSLCList { get; } = new List<SaveLoadClass>();
+        public static List<SaveLoadClass> generalSLCList { get; [Obsolete("It is managed by the Kernel class. Please do not touch it.", false)] internal set; } = new List<SaveLoadClass>();
 
         public class SaveLoadClass
         {
@@ -60,8 +60,9 @@ namespace SCKRM.SaveLoad
             }
         }
 
-        public static void SaveLoadClassLoad()
+        public static void SaveLoadClassLoad<T>(out SaveLoadClass[] result) where T : SaveLoadAttribute
         {
+            List<SaveLoadClass> saveLoadClassList = new List<SaveLoadClass>();
             Assembly[] assemblys = AppDomain.CurrentDomain.GetAssemblies();
             for (int assemblysIndex = 0; assemblysIndex < assemblys.Length; assemblysIndex++)
             {
@@ -69,7 +70,7 @@ namespace SCKRM.SaveLoad
                 for (int typesIndex = 0; typesIndex < types.Length; typesIndex++)
                 {
                     Type type = types[typesIndex];
-                    GeneralSaveLoadAttribute saveLoadAttribute = type.GetCustomAttribute(typeof(GeneralSaveLoadAttribute)) as GeneralSaveLoadAttribute;
+                    T saveLoadAttribute = type.GetCustomAttribute(typeof(T)) as T;
                     if (saveLoadAttribute == null)
                         continue;
 
@@ -104,76 +105,82 @@ namespace SCKRM.SaveLoad
                             fieldInfoList.Add(new SaveLoadClass.SaveLoadVariable<FieldInfo>(fieldInfo, fieldInfo.GetValue(fieldInfo.FieldType)));
                     }
 
-                    generalSLCList.Add(new SaveLoadClass(name, type, propertyInfoList.ToArray(), fieldInfoList.ToArray()));
+                    saveLoadClassList.Add(new SaveLoadClass(name, type, propertyInfoList.ToArray(), fieldInfoList.ToArray()));
                     #endregion
                 }
             }
+
+            result = saveLoadClassList.ToArray();
         }
 
-        public static void Save()
+        public static void Save(List<SaveLoadClass> saveLoadClassList, string saveDataPath)
         {
-            if (!Directory.Exists(Kernel.saveDataPath))
-                Directory.CreateDirectory(Kernel.saveDataPath);
+            if (saveLoadClassList == null || saveDataPath == null || saveDataPath == "")
+                return;
+            else if (!Directory.Exists(saveDataPath))
+                Directory.CreateDirectory(saveDataPath);
 
-            for (int i = 0; i < generalSLCList.Count; i++)
+            for (int i = 0; i < saveLoadClassList.Count; i++)
             {
-                SaveLoadClass variableType = generalSLCList[i];
+                SaveLoadClass saveLoadClass = saveLoadClassList[i];
                 JObject jObject = new JObject();
-                for (int j = 0; j < variableType.propertyInfos.Length; j++)
+                for (int j = 0; j < saveLoadClass.propertyInfos.Length; j++)
                 {
-                    SaveLoadClass.SaveLoadVariable<PropertyInfo> propertyInfo = variableType.propertyInfos[j];
+                    SaveLoadClass.SaveLoadVariable<PropertyInfo> propertyInfo = saveLoadClass.propertyInfos[j];
                     jObject.Add(propertyInfo.variableInfo.Name, JToken.FromObject(propertyInfo.variableInfo.GetValue(propertyInfo.variableInfo.PropertyType)));
                 }
 
-                for (int j = 0; j < variableType.fieldInfos.Length; j++)
+                for (int j = 0; j < saveLoadClass.fieldInfos.Length; j++)
                 {
-                    SaveLoadClass.SaveLoadVariable<FieldInfo> fieldInfo = variableType.fieldInfos[j];
+                    SaveLoadClass.SaveLoadVariable<FieldInfo> fieldInfo = saveLoadClass.fieldInfos[j];
                     jObject.Add(fieldInfo.variableInfo.Name, JToken.FromObject(fieldInfo.variableInfo.GetValue(fieldInfo.variableInfo.FieldType)));
                 }
 
-                File.WriteAllText(PathTool.Combine(Kernel.saveDataPath, variableType.name) + ".json", jObject.ToString());
+                File.WriteAllText(PathTool.Combine(saveDataPath, saveLoadClass.name) + ".json", jObject.ToString());
             }
         }
 
-        public static void Load()
+        public static void Load(List<SaveLoadClass> saveLoadClassList, string loadDataPath)
         {
-            if (!Directory.Exists(Kernel.saveDataPath))
-                Directory.CreateDirectory(Kernel.saveDataPath);
+            if (saveLoadClassList == null || loadDataPath == null || loadDataPath == "")
+                return;
+            else if (!Directory.Exists(loadDataPath))
+                Directory.CreateDirectory(loadDataPath);
 
-            for (int i = 0; i < generalSLCList.Count; i++)
+            for (int i = 0; i < saveLoadClassList.Count; i++)
             {
-                SaveLoadClass variableType = generalSLCList[i];
-                string path = PathTool.Combine(Kernel.saveDataPath, variableType.name) + ".json";
+                SaveLoadClass saveLoadClass = saveLoadClassList[i];
+                string path = PathTool.Combine(loadDataPath, saveLoadClass.name) + ".json";
                 if (!File.Exists(path))
                     continue;
 
                 #region null 설정
-                for (int j = 0; j < variableType.propertyInfos.Length; j++)
+                for (int j = 0; j < saveLoadClass.propertyInfos.Length; j++)
                 {
-                    SaveLoadClass.SaveLoadVariable<PropertyInfo> propertyInfo = variableType.propertyInfos[j];
+                    SaveLoadClass.SaveLoadVariable<PropertyInfo> propertyInfo = saveLoadClass.propertyInfos[j];
                     propertyInfo.variableInfo.SetValue(propertyInfo.variableInfo.PropertyType, null);
                 }
 
-                for (int j = 0; j < variableType.fieldInfos.Length; j++)
+                for (int j = 0; j < saveLoadClass.fieldInfos.Length; j++)
                 {
-                    SaveLoadClass.SaveLoadVariable<FieldInfo> fieldInfo = variableType.fieldInfos[j];
+                    SaveLoadClass.SaveLoadVariable<FieldInfo> fieldInfo = saveLoadClass.fieldInfos[j];
                     fieldInfo.variableInfo.SetValue(fieldInfo.variableInfo.FieldType, null);
                 }
                 #endregion
 
-                JsonConvert.DeserializeObject(File.ReadAllText(path), variableType.type);
+                JsonConvert.DeserializeObject(File.ReadAllText(path), saveLoadClass.type);
 
                 #region json을 로드 했는데도 null이면 기본값으로 되돌리기
-                for (int j = 0; j < variableType.propertyInfos.Length; j++)
+                for (int j = 0; j < saveLoadClass.propertyInfos.Length; j++)
                 {
-                    SaveLoadClass.SaveLoadVariable<PropertyInfo> propertyInfo = variableType.propertyInfos[j];
+                    SaveLoadClass.SaveLoadVariable<PropertyInfo> propertyInfo = saveLoadClass.propertyInfos[j];
                     if (propertyInfo.variableInfo.GetValue(propertyInfo.variableInfo.PropertyType) == null)
                         propertyInfo.variableInfo.SetValue(propertyInfo.variableInfo.PropertyType, propertyInfo.defaultValue);
                 }
 
-                for (int j = 0; j < variableType.fieldInfos.Length; j++)
+                for (int j = 0; j < saveLoadClass.fieldInfos.Length; j++)
                 {
-                    SaveLoadClass.SaveLoadVariable<FieldInfo> fieldInfo = variableType.fieldInfos[j];
+                    SaveLoadClass.SaveLoadVariable<FieldInfo> fieldInfo = saveLoadClass.fieldInfos[j];
                     if (fieldInfo.variableInfo.GetValue(fieldInfo.variableInfo.FieldType) == null)
                         fieldInfo.variableInfo.SetValue(fieldInfo.variableInfo.FieldType, fieldInfo.defaultValue);
                 }
