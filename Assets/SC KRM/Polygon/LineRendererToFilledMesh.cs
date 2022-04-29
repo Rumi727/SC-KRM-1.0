436fdfd048361edc12835ab88fd009cfe5853619
Simsimhan Chobo <jun32820@gmail.com>
2022-04-29 14:56:19 +0900

    라인 렌더러를 채워진 메쉬로 변환하는 스크립트 추가

---
+++ Assets/SC KRM/Polygon/LineRendererToFilledMesh.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+namespace SCKRM.Polygon
+{
+    [ExecuteAlways]
+    [RequireComponent(typeof(MeshFilter))]
+    [RequireComponent(typeof(PolygonCollider2D))]
+    public class LineRendererToFilledMesh : MonoBehaviour
+    {
+        LineRenderer _lineRenderer;
+        public LineRenderer lineRenderer
+        {
+            get
+            {
+                if (_lineRenderer == null)
+                    _lineRenderer = GetComponent<LineRenderer>();
+
+                return _lineRenderer;
+            }
+        }
+
+        MeshFilter _meshFilter;
+        public MeshFilter meshFilter
+        {
+            get
+            {
+                if (_meshFilter == null)
+                    _meshFilter = GetComponent<MeshFilter>();
+
+                return _meshFilter;
+            }
+        }
+
+
+
+        void Awake() => Initialize();
+
+        void Update()
+        {
+            if (meshFilter.sharedMesh == null)
+                Initialize();
+
+            lineRenderer.PositionToFilledMesh(meshFilter.sharedMesh);
+        }
+
+
+
+        public void Initialize()
+        {
+            // This creates a unique mesh per instance. If you re-use shapes
+            // frequently, then you may want to look into sharing them in a pool.
+            Mesh mesh = new Mesh();
+            mesh.MarkDynamic();
+
+            meshFilter.sharedMesh = mesh;
+        }
+    }
+}
