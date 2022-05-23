ed854d7a57af9aa4cdb80119db07ef06bd2e3bf6
Simsimhan Chobo <jun32820@gmail.com>
2022-05-24 04:14:10 +0900

    프로퍼티 이름 변경

--- Assets/SC KRM/Tooltip/Tooltip.cs
+++ Assets/SC KRM/Tooltip/Tooltip.cs
@@ -1,3 +1,4 @@
+using SCKRM.Renderer;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,12 @@ using UnityEngine.EventSystems;
 
 namespace SCKRM.Tooltip
 {
-    public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    public sealed class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         [SerializeField] string _nameSpace; public string nameSpace { get => _nameSpace; set => _nameSpace = value; }
-        [SerializeField] string _tooltip; public string tooltip { get => _tooltip; set => _tooltip = value; }
+        [SerializeField] string _text; public string text { get => _text; set => _text = value; }
 
-
-
-        public void OnPointerEnter(PointerEventData eventData) => TooltipManager.Show(tooltip, nameSpace);
+        public void OnPointerEnter(PointerEventData eventData) => TooltipManager.Show(text, nameSpace);
         public void OnPointerExit(PointerEventData eventData) => TooltipManager.Hide();
     }
 }
