9b4c5d205cb348ebea7ae3626a2437f85edebf28
Simsimhan Chobo <jun32820@gmail.com>
2022-05-25 22:57:21 +0900

    이제 TGA 로더가 RLE로 압축된 이미지를 지원합니다

--- Assets/SC KRM/Resource/ImageLoader.cs
+++ Assets/SC KRM/Resource/ImageLoader.cs
@@ -1,6 +1,3 @@
-// This was made by aaro4130 on the Unity forums.  Thanks boss!
-// It's been optimized and slimmed down for the purpose of loading Quake 3 TGA textures from memory streams.
-
 using System;
 using System.IO;
 using UnityEngine;
@@ -9,6 +6,8 @@ namespace SCKRM.Resource
 {
     public static class ImageLoader
     {
+        // This was made by aaro4130 on the Unity forums.  Thanks boss!
+        // It's been optimized and slimmed down for the purpose of loading Quake 3 TGA textures from memory streams.
         public static Texture2D LoadTGA(string path, bool mipmapUse = false)
         {
             using (BinaryReader r = new BinaryReader(File.OpenRead(path)))
@@ -16,7 +15,13 @@ namespace SCKRM.Resource
                 // Skip some header info we don't care about.
                 // Even if we did care, we have to move the stream seek point to the beginning,
                 // as the previous method in the workflow left it at the end.
-                r.BaseStream.Seek(12, SeekOrigin.Begin);
+                r.BaseStream.Seek(2, SeekOrigin.Begin);
+
+                bool runLength = false;
+                if (r.ReadByte() == 10)
+                    runLength = true;
+
+                r.BaseStream.Seek(9, SeekOrigin.Current);
 
                 short width = r.ReadInt16();
                 short height = r.ReadInt16();
@@ -27,51 +32,165 @@ namespace SCKRM.Resource
 
                 Texture2D tex = new Texture2D(width, height, TextureFormat.RGBA32, mipmapUse);
                 Color32[] pulledColors = new Color32[width * height];
-
-                if (bitDepth == 32)
+                
+                if (!runLength)
                 {
-                    for (int i = 0; i < width * height; i++)
+                    if (bitDepth == 32)
                     {
-                        byte red = r.ReadByte();
-                        byte green = r.ReadByte();
-                        byte blue = r.ReadByte();
-                        byte alpha = r.ReadByte();
+                        for (int i = 0; i < width * height; i++)
+                        {
+                            byte blue = r.ReadByte();
+                            byte green = r.ReadByte();
+                            byte red = r.ReadByte();
+                            byte alpha = r.ReadByte();
 
-                        pulledColors[i] = new Color32(blue, green, red, alpha);
+                            pulledColors[i] = new Color32(red, green, blue, alpha);
+                        }
                     }
-                }
-                else if (bitDepth == 24)
-                {
-                    for (int i = 0; i < width * height; i++)
+                    else if (bitDepth == 24)
                     {
-                        byte red = r.ReadByte();
-                        byte green = r.ReadByte();
-                        byte blue = r.ReadByte();
+                        for (int i = 0; i < width * height; i++)
+                        {
+                            byte blue = r.ReadByte();
+                            byte green = r.ReadByte();
+                            byte red = r.ReadByte();
 
-                        pulledColors[i] = new Color32(blue, green, red, 1);
+                            pulledColors[i] = new Color32(red, green, blue, 255);
+                        }
                     }
-                }
-                else if (bitDepth == 8)
-                {
-                    for (int i = 0; i < width * height; i++)
+                    else if (bitDepth == 8)
                     {
-                        byte red;
-                        byte green;
-                        byte blue;
-                        red = green = blue = r.ReadByte();
+                        for (int i = 0; i < width * height; i++)
+                        {
+                            byte blue;
+                            byte green;
+                            byte red;
+                            blue = green = red = r.ReadByte();
 
-                        pulledColors[i] = new Color32(blue, green, red, 1);
+                            pulledColors[i] = new Color32(red, green, blue, 255);
+                        }
                     }
+                    else
+                        throw new Exception("TGA texture had non 32/24/8 bit depth.");
                 }
-                else
+                else // Code that helped load the compressed TGA (actually it's just a copy paste) https://github.com/EricHaung/TGALoaderForUnity/blob/main/TGALoader.cs
                 {
-                    throw new Exception("TGA texture had non 32/24/8 bit depth.");
+                    if (bitDepth == 32)
+                    {
+                        int index = 0;
+                        while (index < width * height)
+                        {
+                            byte pixelCount = r.ReadByte();
+
+                            if (pixelCount >= 128)
+                            {
+                                pixelCount -= 127;
+
+                                byte blue = r.ReadByte();
+                                byte green = r.ReadByte();
+                                byte red = r.ReadByte();
+                                byte alpha = r.ReadByte();
+
+                                for (int i = 0; i < pixelCount; i++)
+                                    pulledColors[index + i] = new Color32(red, green, blue, alpha);
+                            }
+                            else
+                            {
+                                pixelCount++;
+
+                                for (int i = 0; i < pixelCount; i++)
+                                {
+                                    byte blue = r.ReadByte();
+                                    byte green = r.ReadByte();
+                                    byte red = r.ReadByte();
+                                    byte alpha = r.ReadByte();
+
+                                    pulledColors[index + i] = new Color32(red, green, blue, alpha);
+                                }
+                            }
+
+                            index += pixelCount;
+                        }
+                    }
+                    else if (bitDepth == 24)
+                    {
+                        int index = 0;
+                        while (index < width * height)
+                        {
+                            byte pixelCount = r.ReadByte();
+
+                            if (pixelCount >= 128)
+                            {
+                                pixelCount -= 127;
+
+                                byte blue;
+                                byte green;
+                                byte red;
+                                blue = green = red = r.ReadByte();
+
+                                for (int i = 0; i < pixelCount; i++)
+                                    pulledColors[index + i] = new Color32(red, green, blue, 255);
+                            }
+                            else
+                            {
+                                pixelCount++;
+
+                                for (int i = 0; i < pixelCount; i++)
+                                {
+                                    byte blue;
+                                    byte green;
+                                    byte red;
+                                    blue = green = red = r.ReadByte();
+
+                                    pulledColors[index + i] = new Color32(red, green, blue, 255);
+                                }
+                            }
+
+                            index += pixelCount;
+                        }
+                    }
+                    else if (bitDepth == 8)
+                    {
+                        int index = 0;
+                        while (index < width * height)
+                        {
+                            byte pixelCount = r.ReadByte();
+
+                            if (pixelCount >= 128)
+                            {
+                                pixelCount -= 127;
+
+                                byte blue = r.ReadByte();
+                                byte green = r.ReadByte();
+                                byte red = r.ReadByte();
+
+                                for (int i = 0; i < pixelCount; i++)
+                                    pulledColors[index + i] = new Color32(red, green, blue, 255);
+                            }
+                            else
+                            {
+                                pixelCount++;
+
+                                for (int i = 0; i < pixelCount; i++)
+                                {
+                                    byte blue = r.ReadByte();
+                                    byte green = r.ReadByte();
+                                    byte red = r.ReadByte();
+
+                                    pulledColors[index + i] = new Color32(red, green, blue, 255);
+                                }
+                            }
+
+                            index += pixelCount;
+                        }
+                    }
+                    else
+                        throw new Exception("TGA texture had non 32/24/8 bit depth.");
                 }
 
                 tex.SetPixels32(pulledColors);
                 tex.Apply();
                 return tex;
-
             }
         }
 
