1acdd8b5ea6fc92b8f1726b2422d6fc7aff9abe1
Simsimhan Chobo <jun32820@gmail.com>
2022-06-05 21:57:35 +0900

    SetSizeAsTargetsRectTransform 스크립트 추가

---
+++ Assets/SC KRM/Editor/Inspector/UI/Rect Transform/SetSizeAsTargetsRectTransformEditor.cs
@@ -0,0 +1,43 @@
+using SCKRM.UI;
+using UnityEditor;
+
+namespace SCKRM.Editor
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(SetSizeAsTargetsRectTransform))]
+    public sealed class SetSizeAsTargetsRectTransformEditor : UIEditor
+    {
+        [System.NonSerialized] SetSizeAsTargetsRectTransform editor;
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            editor = (SetSizeAsTargetsRectTransform)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            DrawLine();
+
+            UseProperty("_targetsRectTransform", "대상");
+
+            EditorGUILayout.Space();
+
+            UseProperty("_xSize", "X 크기 변경");
+            UseProperty("_ySize", "Y 크기 변경");
+
+            EditorGUILayout.Space();
+
+            UseProperty("_offset");
+            UseProperty("_min");
+            UseProperty("_max");
+
+            EditorGUILayout.Space();
+
+            UseProperty("_lerp", "애니메이션 사용");
+            if (editor.lerp)
+                UseProperty("_lerpValue", "애니메이션 속도");
+        }
+    }
+}
---
+++ Assets/SC KRM/UI/Rect Transform/SetSizeAsTargetsRectTransform.cs
@@ -0,0 +1,100 @@
+using SCKRM.UI.Layout;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SCKRM.UI
+{
+    [ExecuteAlways]
+    [AddComponentMenu("커널/UI/선택한 Rect Transform들의 크기 따라가기")]
+    [RequireComponent(typeof(RectTransform))]
+    public sealed class SetSizeAsTargetsRectTransform : UIAniLayout
+    {
+        [SerializeField] RectTransform[] _targetRectTransforms;
+        public RectTransform[] targetRectTransforms { get => _targetRectTransforms; set => _targetRectTransforms = value; }
+
+        [SerializeField] bool _xSize = false;
+        public bool xSize { get => _xSize; set => _xSize = value; }
+        [SerializeField] bool _ySize = false;
+        public bool ySize { get => _ySize; set => _ySize = value; }
+
+        [SerializeField] Vector2 _offset = Vector2.zero;
+        public Vector2 offset { get => _offset; set => _offset = value; }
+
+        [SerializeField, Min(0)] Vector2 _min = Vector2.zero;
+        public Vector2 min { get => _min; set => _min = value; }
+        [SerializeField, Min(0)] Vector2 _max = Vector2.zero;
+        public Vector2 max { get => _max; set => _max = value; }
+
+
+
+        DrivenRectTransformTracker tracker;
+
+
+        //protected override void OnEnable() => tracker.Add(this, rectTransform, DrivenTransformProperties.SizeDelta);
+        protected override void OnDisable()
+        {
+            if (!Kernel.isPlaying)
+                tracker.Clear();
+        }
+
+        public override void LayoutRefresh()
+        {
+            if (targetRectTransforms == null)
+                return;
+
+            size = Vector2.zero;
+
+            for (int i = 0; i < targetRectTransforms.Length; i++)
+            {
+                RectTransform targetRectTransform = targetRectTransforms[i];
+                if (targetRectTransform == null)
+                    continue;
+
+                size += new Vector2(targetRectTransform.sizeDelta.x * targetRectTransform.localScale.x, targetRectTransform.sizeDelta.y * targetRectTransform.localScale.y) + offset;
+            }
+
+            if (max.x <= 0)
+                size.x = size.x.Clamp(min.x);
+            else
+                size.x = size.x.Clamp(min.x, max.x);
+            if (max.y <= 0)
+                size.y = size.y.Clamp(min.y);
+            else
+                size.y = size.y.Clamp(min.y, max.y);
+        }
+
+        Vector2 size;
+        public override void SizeUpdate(bool useAni = true)
+        {
+            if (!Kernel.isPlaying)
+            {
+                tracker.Clear();
+
+                if (xSize)
+                    tracker.Add(this, rectTransform, DrivenTransformProperties.SizeDeltaX);
+                if (ySize)
+                    tracker.Add(this, rectTransform, DrivenTransformProperties.SizeDeltaY);
+            }
+
+            if (!lerp || !useAni || !Kernel.isPlaying)
+            {
+                if (xSize && !ySize)
+                    rectTransform.sizeDelta = new Vector2(size.x, rectTransform.sizeDelta.y);
+                else if (!xSize && ySize)
+                    rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, size.y);
+                else if (xSize && ySize)
+                    rectTransform.sizeDelta = size;
+            }
+            else
+            {
+                if (xSize && !ySize)
+                    rectTransform.sizeDelta = rectTransform.sizeDelta.Lerp(new Vector2(size.x, rectTransform.sizeDelta.y), lerpValue * Kernel.fpsUnscaledDeltaTime);
+                else if (!xSize && ySize)
+                    rectTransform.sizeDelta = rectTransform.sizeDelta.Lerp(new Vector2(rectTransform.sizeDelta.x, size.y), lerpValue * Kernel.fpsUnscaledDeltaTime);
+                else if (xSize && ySize)
+                    rectTransform.sizeDelta = rectTransform.sizeDelta.Lerp(size, lerpValue * Kernel.fpsUnscaledDeltaTime);
+            }
+        }
+    }
+}
