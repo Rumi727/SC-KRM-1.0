42277782e91f484a162f0e2401a06c4811b264c0
Simsimhan Chobo <jun32820@gmail.com>
2022-08-01 20:02:39 +0900

    그 망할 경고 좀 치워 제발

--- Assets/SC KRM Test/DiscordController.cs
+++ Assets/SC KRM Test/DiscordController.cs
@@ -5,12 +5,12 @@ using SCKRM;
 
 public class DiscordController : MonoBehaviour
 {
-    long startTime = -1;
-    float timer = 0;
+    /*long startTime = -1;
+    float timer = 0;*/
 
     void Awake()
     {
-        startTime = DateTimeOffset.Now.ToUnixTimeSeconds();
+        //startTime = DateTimeOffset.Now.ToUnixTimeSeconds();
 
         string version;
 #if UNITY_EDITOR
