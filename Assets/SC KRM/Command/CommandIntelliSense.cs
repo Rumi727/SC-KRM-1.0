2d224d2a41df5734f8c0db20b80f39266781776f
SimsimhanChobo <jun32820@gmail.com>
2022-08-24 17:11:11 +0900

    안쓰는 스크립트, using 삭제

--- Assets/SC KRM/Command/CommandIntelliSense.cs
+++ Assets/SC KRM/Command/CommandIntelliSense.cs
@@ -2,7 +2,6 @@ using Brigadier.NET;
 using Brigadier.NET.Context;
 using Brigadier.NET.Exceptions;
 using Brigadier.NET.Tree;
-using SCKRM.Input;
 using SCKRM.Object;
 using SCKRM.Text;
 using SCKRM.UI;
@@ -10,7 +9,6 @@ using SCKRM.UI.Layout;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 namespace SCKRM.Command
