ad892aa965915a8a3f7d13e21e7914f002675dad
SimsimhanChobo <jun32820@gmail.com>
2022-08-28 03:07:40 +0900

    쓸모 없는 이벤트 삭제

--- Assets/SC KRM/InitialLoadManager.cs
+++ Assets/SC KRM/InitialLoadManager.cs
@@ -29,10 +29,6 @@ namespace SCKRM
         public static bool isInitialLoadEnd { get; private set; } = false;
         public static bool isSceneMoveEnd { get; private set; } = false;
 
-        public static event Action initialLoadStart;
-        public static event Action initialLoadEnd;
-        public static event Action initialLoadEndSceneMove;
-
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
         static async UniTaskVoid InitialLoad()
         {
@@ -45,7 +41,6 @@ namespace SCKRM
             {
                 //초기로딩이 시작됬습니다
                 isInitialLoadStart = true;
-                initialLoadStart?.Invoke();
 
                 //이 함수는 어떠한 경우에도 메인스레드가 아닌 스레드에서 실행되면 안됩니다
                 if (!ThreadManager.isMainThread)
@@ -190,7 +185,6 @@ namespace SCKRM
                 {
                     //초기 로딩이 끝났습니다
                     isInitialLoadEnd = true;
-                    initialLoadEnd?.Invoke();
 
                     Debug.Log("Kernel: Initial loading finished!");
                 }
@@ -233,7 +227,6 @@ namespace SCKRM
 
                     //씬을 이동했으면 이벤트를 호출합니다
                     isSceneMoveEnd = true;
-                    initialLoadEndSceneMove?.Invoke();
                 }
             }
             catch (Exception e)
