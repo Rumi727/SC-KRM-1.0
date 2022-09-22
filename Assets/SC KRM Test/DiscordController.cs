37d448f292c24b2cf67cbbef7f929dd8b7db0dd0
SimsimhanChobo <jun32820@gmail.com>
2022-09-22 18:35:54 +0900

    Revert "디스코드 API 엔진 변경"
    This reverts commit e9d184d535a004ab206d4a14950dd8b70645161c.

--- Assets/SC KRM Test/DiscordController.cs
+++ Assets/SC KRM Test/DiscordController.cs
@@ -1,24 +1,24 @@
 using System;
 using UnityEngine;
-//using DiscordPresence;
+using DiscordPresence;
 using SCKRM;
 
 public class DiscordController : MonoBehaviour
 {
-    /*long startTime = -1;
-    float timer = 0;*/
+    long startTime = -1;
+    //float timer = 0;
 
     void Awake()
     {
-        //startTime = DateTimeOffset.Now.ToUnixTimeSeconds();
+        startTime = DateTimeOffset.Now.ToUnixTimeSeconds();
 
         string version;
 #if UNITY_EDITOR
         version = $"{Kernel.productName} {Kernel.version} | Unity {Kernel.unityVersion}";
 #else
-            version = $"{Kernel.productName} {Kernel.version}";
+        version = $"{Kernel.productName} {Kernel.version}";
 #endif
 
-        //PresenceManager.UpdatePresence("Simsimhan Chobo Kernel Manager", "Rich Presence Test", startTime, PresenceManager.instance.presence.endTimestamp, PresenceManager.instance.presence.largeImageKey, version);
+        PresenceManager.UpdatePresence("Simsimhan Chobo Kernel Manager", "Rich Presence Test", startTime, PresenceManager.instance.presence.endTimestamp, PresenceManager.instance.presence.largeImageKey, version);
     }
 }
